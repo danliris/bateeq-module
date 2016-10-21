e20eb8a755a3e87a7709205519527575ad1c242e
lioenel <kepin91@yahoo.co.id>
2016-10-21 11:15:57 +0700

    add validation for POS payment

--- src/managers/pos/pos-payment-doc-manager.js
+++ src/managers/pos/pos-payment-doc-manager.js
@@ -17,6 +17,22 @@ module.exports = class PaymentManager {
         this.db = db;
         this.user = user;
         this.paymentCollection = this.db.use(map.pos.PaymentDoc);
+        
+        var ArticleVariantManager = require('../core/article/article-variant-manager');
+        this.articleVariantManager = new ArticleVariantManager(db, user);
+        
+        var StoreManager = require('../inventory/store-manager');
+        this.storeManager = new StoreManager(db, user); 
+        
+        var BankManager = require('../pos-master/bank-manager');
+        this.bankManager = new BankManager(db, user);  
+        
+        var CardTypeManager = require('../pos-master/card-type-manager');
+        this.cardTypeManager = new CardTypeManager(db, user);  
+        
+        var PaymentTypeManager = require('../pos-master/payment-type-manager');
+        this.paymentTypeManager = new PaymentTypeManager(db, user);  
+        
     }
 
     read(paging) {
@@ -201,8 +217,23 @@ module.exports = class PaymentManager {
     _validate(payment) {
         var errors = {};
         return new Promise((resolve, reject) => {
-            var valid = new Payment(payment);
-            // 1. begin: Declare promises.
+            var valid = new Payment(payment); 
+            
+            var paymentDetailError = {};
+            if (!valid.code || valid.code == '')
+                errors["code"] = "code is required";
+            if (!payment.storeId || payment.storeId == '')
+                errors["storeId"] = "storeId is required"; 
+            if (!payment.paymentDetail.paymentTypeId || payment.paymentDetail.paymentTypeId == '')
+                paymentDetailError["paymentTypeId"] = "paymentTypeId is required";
+            
+            for (var prop in paymentDetailError) {
+                errors["paymentDetail"] = paymentDetailError;
+                break;
+            }
+                                
+
+            //Get Payment data
             var getPayment = this.paymentCollection.singleOrDefault({
                 "$and": [{
                     _id: {
@@ -211,30 +242,196 @@ module.exports = class PaymentManager {
                 }, {
                         code: valid.code
                     }]
-            });
-            // 1. end: Declare promises.
-
-            // 2. begin: Validation.
-            Promise.all([getPayment])
-                .then(results => {
+            });  
+            var getStore = this.storeManager.getByIdOrDefault(payment.storeId);
+            var getBank = this.bankManager.getByIdOrDefault(payment.paymentDetail.bankId);
+            var getCardType = this.cardTypeManager.getByIdOrDefault(payment.paymentDetail.cardTypeId);
+            var getPaymentType = this.paymentTypeManager.getByIdOrDefault(payment.paymentDetail.paymentTypeId);
+            var getVoucher = Promise.resolve(null);
+            var getItems = [];
+            if (valid.items && valid.items.length > 0) {
+                for (var item of valid.items) {  
+                    getItems.push(this.articleVariantManager.getByIdOrDefault(item.articleVariantId));
+                }
+            }
+            else {
+                errors["items"] = "items is required";
+            }
+            
+            Promise.all([getPayment, getStore, getBank, getCardType, getPaymentType, getVoucher].concat(getItems))
+               .then(results => {
                     var _payment = results[0];
+                    var _store = results[1];
+                    var _bank = results[2];
+                    var _cardType = results[3];
+                    var _paymentType = results[4];
+                    var _voucherType = results[5];
 
-                    if (!valid.code || valid.code == '')
-                        errors["code"] = "code is required";
-                    else if (_payment) {
+                    if (_payment) {
                         errors["code"] = "code already exists";
+                    }  
+                    
+                    if (!_store) {
+                        errors["storeId"] = "storeId not found";
+                    }
+                    else {
+                        valid.storeId = _store._id;
+                        valid.store = _store;
+                    } 
+                      
+                    valid.totalProduct = 0;
+                    valid.subTotal = 0;
+                    valid.grandTotal = 0;
+                    var articleVariants = results.slice(6, results.length) 
+                    if (articleVariants.length > 0) {
+                        var itemErrors = [];
+                        for (var variant of articleVariants) {
+                            var index = articleVariants.indexOf(variant);
+                            var item = valid.items[index];
+                            var itemError = {};
+
+                            if (!item.articleVariantId || item.articleVariantId == '') {
+                                itemError["articleVariantId"] = "articleVariantId is required";
+                            }
+                            else {
+                                for (var i = valid.items.indexOf(item) + 1; i < valid.items.length; i++) {
+                                    var otherItem = valid.items[i];
+                                    if (item.articleVariantId == otherItem.articleVariantId) {
+                                        itemError["articleVariantId"] = "articleVariantId already exists on another detail";
+                                    }
+                                }
+                            }
+                            if (!variant) {
+                                itemError["articleVariantId"] = "articleVariantId not found";
+                            }
+                            else {
+                                item.articleVariantId = variant._id;
+                                item.articleVariant = variant;
+                                if(variant.size)
+                                    if(variant.size.name)
+                                        item.size = variant.size.name;
+                                item.price = parseInt(variant.domesticSale);
+                            }
+
+                            if (item.quantity == undefined || (item.quantity && item.quantity == '')) {
+                                itemError["quantity"] = "quantity is required";
+                                item.quantity = 0;
+                            }
+                            else if (parseInt(item.quantity) <= 0) {
+                                itemError["quantity"] = "quantity must be greater than 0";
+                            } 
+                            
+                            item.total = (parseInt(item.quantity) * parseInt(item.price)) * (1 - (parseInt(item.discount1) / 100)) * (1 - (parseInt(item.discount2) / 100)) * (1 - (parseInt(item.specialDiscount) / 100))
+                            valid.subTotal = parseInt(valid.subTotal) + parseInt(item.total);
+                            valid.totalProduct = parseInt(valid.totalProduct) + parseInt(item.quantity);
+                            itemErrors.push(itemError);
+                        }
+                        var totalDiscount = parseInt(valid.subTotal) * parseInt(valid.discount) / 100;
+                        var totalVoucher = 0;
+                        valid.grandTotal = parseInt(valid.subTotal) - parseInt(totalDiscount) - parseInt(totalVoucher);
+                        
+                        for (var itemError of itemErrors) {
+                            for (var prop in itemError) {
+                                errors.items = itemErrors;
+                                break;
+                            }
+                            if (errors.items)
+                                break;
+                        }
                     } 
                     
-                    // 2c. begin: check if data has any error, reject if it has.
+                    if (!_paymentType) {
+                        paymentDetailError["paymentTypeId"] = "paymentTypeId not found";
+                    }
+                    else {
+                        valid.paymentDetail.paymentTypeId = _paymentType._id;
+                        valid.paymentDetail.paymentType = _paymentType;
+                        
+                        if(_paymentType.name.toLowerCase() == "card" || _paymentType.name.toLowerCase() == "partial"){
+                            if (!payment.paymentDetail.bankId || payment.paymentDetail.bankId == '')
+                                paymentDetailError["bankId"] = "bankId is required";
+                            if (!_bank) {
+                                paymentDetailError["bankId"] = "bankId not found";
+                            }
+                            else {
+                                valid.paymentDetail.bankId = _bank._id;
+                                valid.paymentDetail.bank = _bank;
+                            }
+                            
+                            if (!payment.paymentDetail.cardTypeId || payment.paymentDetail.cardTypeId == '')
+                                paymentDetailError["cardTypeId"] = "cardTypeId is required"; 
+                            if (!_cardType) {
+                                paymentDetailError["cardTypeId"] = "cardTypeId not found";
+                            }
+                            else {
+                                valid.paymentDetail.cardTypeId = _cardType._id;
+                                valid.paymentDetail.cardType = _cardType;
+                            }   
+                            
+                            if (!valid.paymentDetail.card || valid.paymentDetail.card == '')
+                                paymentDetailError["card"] = "card is required";
+                            else {
+                                if(valid.paymentDetail.card.toLowerCase() != 'debit' && valid.paymentDetail.card.toLowerCase() != 'credit')
+                                    paymentDetailError["card"] = "card must be debit or credit"; 
+                            }
+                                
+                            if (!valid.paymentDetail.cardNumber || valid.paymentDetail.cardNumber == '')
+                                paymentDetailError["cardNumber"] = "cardNumber is required";
+                                
+                            if (!valid.paymentDetail.cardName || valid.paymentDetail.cardName == '')
+                                paymentDetailError["cardName"] = "cardName is required"; 
+                                
+                            if (valid.paymentDetail.cardAmount == undefined || (valid.paymentDetail.cardAmount && valid.paymentDetail.cardAmount == '')) {
+                                paymentDetailError["cardAmount"] = "cardAmount is required";
+                                valid.paymentDetail.cardAmount = 0;
+                            } 
+                            else if(parseInt(valid.paymentDetail.cardAmount) <= 0) {
+                                paymentDetailError["cardAmount"] = "cardAmount must be greater than 0";
+                            }  
+                        }  
+                        
+                        if(_paymentType.name.toLowerCase() == "cash" || _paymentType.name.toLowerCase() == "partial"){ 
+                            if (valid.paymentDetail.cashAmount == undefined || (valid.paymentDetail.cashAmount && valid.paymentDetail.cashAmount == '')) {
+                                paymentDetailError["cashAmount"] = "cashAmount is required";
+                                valid.paymentDetail.cashAmount = 0;
+                            } 
+                            else if(parseInt(valid.paymentDetail.cashAmount) <= 0) {
+                                paymentDetailError["cashAmount"] = "cashAmount must be greater than 0";
+                            } 
+                        } 
+                        
+                        if(_paymentType.name.toLowerCase() == "partial")
+                            if((valid.paymentDetail.cashAmount + valid.paymentDetail.cardAmount) < valid.grandTotal)
+                                errors["grandTotal"] = "grandTotal is bigger than payment";  
+                                
+                        if(_paymentType.name.toLowerCase() == "card")
+                            if(valid.paymentDetail.cardAmount < valid.grandTotal)
+                                errors["grandTotal"] = "grandTotal is bigger than payment";  
+                                
+                        if(_paymentType.name.toLowerCase() == "cash")
+                            if(valid.paymentDetail.cashAmount < valid.grandTotal)
+                                errors["grandTotal"] = "grandTotal is bigger than payment";  
+                    } 
+                    
+                    for (var prop in paymentDetailError) {
+                        errors["paymentDetail"] = paymentDetailError;
+                        break;
+                    }
+            
                     for (var prop in errors) {
                         var ValidationError = require('../../validation-error');
                         reject(new ValidationError('data does not pass validation', errors));
                     }
 
+                    valid = new Payment(valid);
                     valid.stamp(this.user.username, 'manager');
                     resolve(valid);
                 })
-                .catch(e => {
+                .catch(e => { 
+                    for (var prop in errors) {
+                        var ValidationError = require('../../validation-error');
+                        reject(new ValidationError('data does not pass validation', errors));
+                    } 
                     reject(e);
                 })
         });
