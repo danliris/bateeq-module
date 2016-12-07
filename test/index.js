b6a07b02cbfc4de62c8a035360b6e470d04291fd
Leslie Aula <cristiano.ulo@gmail.com>
2016-12-07 18:53:25 +0700

    merge

--- test/index.js
+++ test/index.js
@@ -9,20 +9,17 @@ describe('#bateeq-module', function (done) {
     this.timeout(2 * 60000);
     // //auth
     // test('@auth/account-manager', './auth/account-manager-test');
-<<<<<<< HEAD
     // test('@auth/role-manager', './auth/role-manager-test');
     
     // test('@manager/master/item-manager', './managers/master/item-manager-test');
     // test('@manager/master/material-manager', './managers/master/material-manager-test');
     // test('@manager/master/finished-goods-manager', './managers/master/finished-goods-manager-test');
     
-=======
     // test('@auth/role-manager', './auth/role-manager-test'); 
     // test('@manager/master/item-manager', './managers/master/item-manager-test');
     // test('@manager/master/material-manager', './managers/master/material-manager-test');
     // test('@manager/master/finished-goods-manager', './managers/master/finished-goods-manager-test');
 
->>>>>>> refs/remotes/upstream/dev
     // test('@manager/article-brand-manager', './managers/article-brand-manager-test');
     // test('@manager/article-category-manager', './managers/article-category-manager-test');
     // test('@manager/article-counter-manager', './managers/article-counter-manager-test');
@@ -33,15 +30,14 @@ describe('#bateeq-module', function (done) {
     // test('@manager/article-sub-counter-manager', './managers/article-sub-counter-manager-test');
     // test('@manager/article-theme-manager', './managers/article-theme-manager-test');
     // test('@manager/article-type-manager', './managers/article-type-manager-test');
-<<<<<<< HEAD
     // test('@manager/article-manager', './managers/article-manager-test');
     
     // test('@manager/storage-manager', './managers/storage-manager-test');
-=======
+
     // test('@manager/article-manager', './managers/article-manager-test'); 
 
     // test('@manager/storage-manager', './managers/storage-manager-test'); 
->>>>>>> refs/remotes/upstream/dev
+
     // test('@manager/store-manager', './managers/store-manager-test');
     // test('@manager/supplier-manager', './managers/supplier-manager-test');
     // test('@manager/transfer-in-doc-manager', './managers/transfer-in-doc-manager-test');
@@ -50,11 +46,6 @@ describe('#bateeq-module', function (done) {
     // test('@manager/transfer-in-doc-ext-manager', './managers/transfer-in-doc-ext-manager-test');
     // test('@manager/inventory-manager', './managers/inventory-manager-test');
     // test('@manager/module-manager', './managers/module-manager-test');
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> refs/remotes/upstream/dev
     // test('@manager/efr-kb-rtt-manager', './managers/efr-kb-rtt-manager-test');
     // test('@manager/efr-kb-fng-manager', './managers/efr-kb-fng-manager-test');
     // test('@manager/efr-kb-rtp-manager', './managers/efr-kb-rtp-manager-test');
@@ -74,7 +65,7 @@ describe('#bateeq-module', function (done) {
     // test('@manager/efr-tb-sab-manager', './managers/efr-tb-sab-manager-test');
     // test('@manager/efr-kb-exb-manager', './managers/efr-kb-exb-manager-test');
     // test('@manager/efr-hp-fng-manager', './managers/efr-hp-fng-manager-test'); 
-<<<<<<< HEAD
+
     
     // test('@manager/bank-manager', './managers/bank-manager-test'); 
     // test('@manager/card-type-manager', './managers/card-type-manager-test'); 
@@ -88,7 +79,7 @@ describe('#bateeq-module', function (done) {
     // test("@manager/master/finished-goods/basic", "./managers/master/finished-goods/index");
     // test("@manager/master/item/basic", "./managers/master/item/index");
     test("@manager/sales/promo", "./managers/sales/promo/index");
-=======
+
 
     // test('@manager/bank-manager', './managers/bank-manager-test'); 
     // test('@manager/card-type-manager', './managers/card-type-manager-test'); 
@@ -108,5 +99,5 @@ describe('#bateeq-module', function (done) {
     test("@manager/master/card-type", "./managers/master/card-type");
     test("@manager/master/finished-goods", "./managers/master/finished-goods");
     test("@manager/master/item", "./managers/master/item");
->>>>>>> refs/remotes/upstream/dev
+
 })
