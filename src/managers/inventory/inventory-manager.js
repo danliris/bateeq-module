f0686bf44e1edf181fc61cf878ba645b512a7225
danliris <danliris.cloud@gmail.com>
2020-04-22 18:54:59 +0700

    Update inventory-manager.js

--- src/managers/inventory/inventory-manager.js
+++ src/managers/inventory/inventory-manager.js
@@ -729,6 +729,15 @@ module.exports = class InventoryManager extends BaseManager {
                     });
 
                     inventory.quantity += quantity;
+                
+                    if (inventory.storage.code == 'GDG.05' && inventory.quantity < 0) {
+                        movement.before = (originQuantity - quantity)
+                        movement.after = (movement.before + quantity)
+                        inventory.quantity = 0
+                    }
+                     else {
+                        movement.before = originQuantity
+                     }
 
                     var updateInventory = this.update(inventory);
                     var createMovement = this.inventoryMovementManager.create(movement);
